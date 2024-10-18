9d6ae6369bc68d7851abcc1006ea611c175b37b1
Sagar-rm <sagarrathod292007@gmail.com>
2024-10-18 18:23:54 +0530

    Data types added

---
+++ 01 basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs , not browser
+
+console.log( 3 + 3); 
+//code readablity should be clean
+console.log("hello")
+
+let name = "Sagar";
+let age = 17;
+let state = null;
+
+//primitive data types
+// number => 2 to the power of 53
+// biginit
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof null) // output => object
+console.log(typeof "Sgar") // output => String
+console.log(typeof 45) // output => number
+console.log(typeof undefined) // output => undefined
