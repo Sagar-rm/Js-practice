b5a7f6d4445dea27e316cca97bf977dc3bf24045
Sagar-rm <sagarrathod292007@gmail.com>
2024-10-20 16:02:19 +0530

    Opreations added

--- 01 basics/03_coversion-opreations.js
+++ 01 basics/03_coversion-opreations.js
@@ -1,11 +1,11 @@
 let score = undefined
 
-console.log(typeof score);
-console.log(typeof (score))
+// console.log(typeof score);
+// console.log(typeof (score))
 
 let valueInNumner = Number(score);
-console.log(typeof valueInNumner)
-console.log(valueInNumner)
+// console.log(typeof valueInNumner)
+// console.log(valueInNumner)
 
 // "32" => 32
 // "32as" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumner)
 
 let isLoggedIn = null
 let booleanIsLogged = Boolean(isLoggedIn );
-console.log(booleanIsLogged)
+// console.log(booleanIsLogged)
 
 // 1 => true 0 => false
 // "" => false
@@ -22,5 +22,45 @@ console.log(booleanIsLogged)
 
 let someNumber = 2;
 let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// **************************Operations**************************
+
+let value = 3;
+let negValue = -value
+console.log(negValue)
+
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2-2);
+
+let str1 = "Hello";
+let str2 = " Sagar"
+
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2);
+// console.log( 1 + 2 + "2")
+
+// console.log( 5 * 6 % 4 - 2 + 1)
+
+
+console.log(+true)
+console.log(+"")
+
+let num1 , num2 , num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
